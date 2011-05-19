b821ccc9edadeb16c40a258119a198a4f67bfee8
John Bragg <remotezygote@gmail.com>
2011-05-19 13:01:26 -0700

    paper style coming along

--- static/javascripts/touchyfeely.js
+++ static/javascripts/touchyfeely.js
@@ -7,40 +7,40 @@
 var TouchyFeely = function(element,options) {
 	var touchy = this;
 	var TouchHooks = {
-		touchstart : function(event) {
+		start : function(event) {
 			if(event.touches.length == 1) {
 				touchy.update(event,false);
-				touchy.currentEvent.target = touchy.getTarget(event);
+				// touchy.cE.trg = touchy.trg(event);
 			};
 		},
-		touchmove : function(event) {
-			if(touchy.currentEvent) {
+		move : function(event) {
+			if(touchy.cE) {
 				touchy.update(event);
-				if(touchy.currentEvent.type) { TouchEvent(touchy.currentEvent.type+"move"); };
+				if(touchy.cE.type) { TouchEvent(touchy.cE.type+"move"); };
 				return(false);
 			};
 			return(true);
 		},
-		touchend : function(event) {
-			if(touchy.currentEvent) {
+		end : function(event) {
+			if(touchy.cE) {
 				touchy.update(event);
-				if(touchy.currentEvent.type) { TouchEvent(touchy.currentEvent.type+"end"); };
+				if(touchy.cE.type) { TouchEvent(touchy.cE.type+"end"); };
 			};
 			touchy.cancel();
 		},
-		touchcancel : function(event) { touchy.cancel(); }
+		cancel : function(event) { touchy.cancel(); }
 	};
 	var TouchEvent = function(type) {
 		var evObj = document.createEvent('UIEvents');
 		evObj.initUIEvent(type, true, true, window, 1);
-		for(var i in touchy.currentEvent) { evObj[i] = touchy.currentEvent[i]; };
-		touchy.element.dispatchEvent(evObj);
+		for(var i in touchy.cE) { evObj[i] = touchy.cE[i]; };
+		touchy.el.dispatchEvent(evObj);
 		// console.log("Event: "+type,evObj);
 	};
-	var baseEvent = {
-		history : []
+	var bE = {
+		h : []
 	};
-	touchy.element = element;
+	touchy.el = element;
 	touchy.config = {
 		minLength : 20,
 		capturescroll : true,
@@ -58,68 +58,68 @@ var TouchyFeely = function(element,options) {
 	  };
 		return newObj;
 	};
-	touchy.hook = function(elem) { for(var i in TouchHooks) { elem.addEventListener(i, TouchHooks[i], true); }; };
-	touchy.swipeLength = function() {
-		return(Math.round(Math.sqrt(Math.pow(touchy.currentEvent.deltaX,2)+Math.pow(touchy.currentEvent.deltaY,2))));
+	touchy.hook = function(elem) { for(var i in TouchHooks) { elem.addEventListener("touch"+i, TouchHooks[i], true); }; };
+	touchy.length = function() {
+		return(Math.round(Math.sqrt(Math.pow(touchy.cE.dX,2)+Math.pow(touchy.cE.dY,2))));
 	};
-	touchy.calculateAngle = function() {
-		var ang = Math.round((Math.atan2(touchy.currentEvent.vertDiff,touchy.currentEvent.horzDiff))*180/Math.PI); //angle in degrees
+	touchy.ang = function() {
+		var ang = Math.round((Math.atan2(touchy.cE.vD,touchy.cE.hD))*180/Math.PI); //angle in degrees
 		if(ang < 0) ang = 360 - Math.abs(ang);
 		return(ang);
 	};
-	touchy.swipeDirection = function() {
-		if ( ((this.currentEvent.swipeAngle <= 45) && (this.currentEvent.swipeAngle >= 0)) || ((this.currentEvent.swipeAngle <= 360) && (this.currentEvent.swipeAngle >= 315)) ) {
+	touchy.dir = function() {
+		if ( ((touchy.cE.angle <= 45) && (touchy.cE.angle >= 0)) || ((touchy.cE.angle <= 360) && (touchy.cE.angle >= 315)) ) {
 			return('left');
-		} else if ( (this.currentEvent.swipeAngle >= 135) && (this.currentEvent.swipeAngle <= 225) ) {
+		} else if ( (touchy.cE.angle >= 135) && (touchy.cE.angle <= 225) ) {
 			return('right');
-		} else if ( (this.currentEvent.swipeAngle > 45) && (this.currentEvent.swipeAngle < 135) ) {
+		} else if ( (touchy.cE.angle > 45) && (touchy.cE.angle < 135) ) {
 			return('down');
 		};
 		return('up');
 	};
-	touchy.getType = function() {
-		var cev = touchy.currentEvent;
-		if(cev.swipeDirection=="left"||cev.swipeDirection=="right") return("swipe");
-		else if(cev.swipeDirection=="up"||cev.swipeDirection=="down") return("scroll");
+	touchy.type = function() {
+		var cev = touchy.cE;
+		if(cev.direction=="left"||cev.direction=="right") return("swipe");
+		else if(cev.direction=="up"||cev.direction=="down") return("scroll");
 		else return(undefined);
 	};
-	touchy.getTarget = function(event) {
+	touchy.trg = function(event) {
 		return(event.currentTarget);
 	};
 	touchy.update = function(event,capture) {
-		if(!touchy.currentEvent) touchy.newEvent();
-		var tch = event["touches"][0] || event["changedTouches"][0], cev = touchy.currentEvent;
-		cev.curX = tch.pageX;
-		cev.curY = tch.pageY;
-		cev.history = cev.history || [];
-		cev.history.push([cev.curX,cev.curY]);
-		if(!cev.fingerCount) cev.fingerCount = event["touches"].length;
-		if(!cev.startX) cev.startX = cev.curX;
-		if(!cev.startY) cev.startY = cev.curY;
-		cev.deltaX = cev.curX - cev.startX;
-		cev.deltaY = cev.curY - cev.startY;
-		cev.horzDiff = cev.startX - cev.curX;
-		cev.vertDiff = cev.curY - cev.startY;
-		if(cev.deltaX > 0 || cev.deltaY > 0) {
-			cev.swipeLength = touchy.swipeLength();
-			if(cev.swipeLength >= touchy.config.minLength) {
-				cev.swipeAngle = touchy.calculateAngle();
-				cev.swipeDirection = touchy.swipeDirection();
+		var tch = event["touches"][0] || event["changedTouches"][0], cev = touchy.cE || touchy.nE();
+		cev.cX = tch.pageX;
+		cev.cY = tch.pageY;
+		cev.h = cev.h || [];
+		cev.h.push([cev.cX,cev.cY]);
+		if(!cev.fC) cev.fC = event["touches"].length;
+		if(!cev.sX) cev.sX = cev.cX;
+		if(!cev.sY) cev.sY = cev.cY;
+		cev.dX = cev.cX - cev.sX;
+		cev.dY = cev.cY - cev.sY;
+		cev.hD = cev.sX - cev.cX;
+		cev.vD = cev.cY - cev.sY;
+		if(cev.dX > 0 || cev.dY > 0) {
+			cev.length = touchy.length();
+			if(cev.length >= touchy.config.minLength) {
+				cev.angle = touchy.ang();
+				cev.direction = touchy.dir();
 				if(!cev.type) {
-					cev.type = touchy.getType();
+					cev.type = touchy.type();
 					if(cev.type) TouchEvent(cev.type+"start");
 				};
 			} else {
-				cev.swipeAngle = undefined;
-				cev.swipeDirection = undefined;
+				cev.angle = undefined;
+				cev.direction = undefined;
 			};
 		};
 		if(capture && (!cev.type || touchy.config["capture"+cev.type]==true)) event.preventDefault();
 		// if(cev.type) console.log(touchy.config["capture"+cev.type]+"");
 	};
-	touchy.newEvent = function() { delete touchy["currentEvent"]; touchy.currentEvent = touchy.clone(touchy.baseEvent); };
-	touchy.cancel = function() { delete touchy["currentEvent"]; };
-	touchy.currentEvent = false;
+	touchy.nE = function() { delete touchy["cE"]; touchy.cE = touchy.clone(touchy.bE); return(touchy.cE); };
+	touchy.cancel = function() { delete touchy["cE"]; };
+	touchy.cE = false;
+	touchy.currentEvent = touchy.cE;
 	touchy.hook(element);
 	return(touchy);
 };
