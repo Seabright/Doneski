d628cdde8fad536908a4790a55cb4c4016c77c6d
John Bragg <remotezygote@gmail.com>
2011-10-04 14:31:23 -0700

    b64 apple icons

--- lib/seabright/stylesheet.rb
+++ lib/seabright/stylesheet.rb
@@ -2,8 +2,12 @@ module Seabright
   class Stylesheet
     def self.minifies?(paths) !paths.grep(%r[\.css(\?\d+)?$]).empty?; end
 
-    def initialize(content)
+    def initialize(content=nil)
       @content = content.nil? ? nil : minify(content)
+    end
+    
+    def self.b64(image_path)
+      "data:image/png;base64,#{Base64.encode64(IO.read("static#{image_path}")).gsub("\n",'')}"
     end
     
     def minified; @content; end
@@ -13,7 +17,7 @@ module Seabright
       content.compress_whitespace.remove_comments.remove_spaces_outside_block.
         remove_spaces_inside_block.trim_last_semicolon.encode_images.strip
     end
-
+    
     module Minifier
       def compress_whitespace; compress!(/\s+/, ' '); end
       def remove_comments; compress!(/\/\*.*?\*\/\s?/, ''); end
@@ -28,8 +32,11 @@ module Seabright
       def trim_last_semicolon; compress!(/;(?=\})/, ''); end
       def encode_images
         compress!(/\{(.*?)(?=\})/) do |m|
-          m.gsub(/\burl\(([^\)]+)\)/) { |n| "url(data:image/png;base64,#{Base64.encode64(IO.read("static#{$1}")).gsub("\n",'')})" }.strip
+          m.gsub(/\burl\(([^\)]+)\)/) { |n| "url(#{b64($1)})" }.strip
         end
+      end
+      def b64(image_path)
+        "data:image/png;base64,#{Base64.encode64(IO.read("static#{image_path}")).gsub("\n",'')}"
       end
     private
       def compress!(*args, &block) gsub!(*args, &block) || self; end
